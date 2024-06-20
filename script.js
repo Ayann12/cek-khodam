9e85e8964515355047d5530f0538353ceb87fc80
Ayann12 <tantowi818@gmail.com>
2024-06-21 00:37:14 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+document.getElementById('khodamForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+    const name = document.getElementById('nameInput').value.trim();
+    const resultDiv = document.getElementById('result');
+
+    // Data khodam yang di-hardcode
+    const khodamData = [
+        'Khodam Api',
+        'Khodam Air',
+        'Khodam Angin',
+        'Khodam Bumi',
+        'Khodam Cahaya',
+        'Khodam Kegelapan',
+        'Khodam Petir',
+        'Khodam Hutan',
+        'Khodam Laut',
+        'Khodam Gunung',
+        'Khodam Langit',
+        'Khodam Batu',
+        'Khodam Matahari',
+        'Khodam Bulan',
+        'Khodam Bintang',
+        'Khodam Seblak',
+        'Khodam Pecel',
+        'Khodam Caper',
+        'Khodam Mie Ayam',
+        'Khodam Ganteng',
+        'Khodam Bakwan',
+
+    ];
+
+    // Fungsi untuk mendapatkan khodam secara acak
+    function getRandomKhodam() {
+        const randomIndex = Math.floor(Math.random() * khodamData.length);
+        return khodamData[randomIndex];
+    }
+
+    const khodam = getRandomKhodam();
+
+    // Tampilkan hasil
+    resultDiv.classList.remove('d-none', 'alert-danger', 'alert-success');
+    resultDiv.classList.add('alert-success');
+    resultDiv.textContent = `Nama : ${name}, Terdapat : ${khodam}`;
+});
